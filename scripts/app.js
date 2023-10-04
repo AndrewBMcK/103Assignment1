85951df6513765001ea4dba042ed2af52b3770bf
AndrewBMcK <andrewbmckinnon@outlook.com>
2023-10-04 15:25:18 -0400

    second attempt

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+//Assignment 1
+var welcome = "Welcome! ";
+var currentInfo = "This is your current info, ";
+var fName = "Andrew ";
+var lName = "McKinnon";
+var email = "andrewbmckinnon@outlook.com";
+var birthDate = "12/03/1988";
+var height = "5'8";
+var myAge = 34;
+var uWeight = "157 lbs";
+var locationCity = "Savannah ";
+var locationState = "GA ";
+var zip = 31401;
+var housingType = "Apartment";
+var occupation = "Student";
+var localTime = "2:10PM";
+var currentDate = " 10/04/2023";
+var prevEmployment = "Maximus";
+var prevJobtitle = "Admin 2";
+var otherExperience = "USMC Infantry";
+var goodbye = "Call us if you have any questions!";
+
+document.write(welcome + currentInfo + "your name is registered as " + fName + lName);
+document.write("<br>");
+document.write("<br>");
+document.write("Your current email address is " + email + " and your birthday is: " + birthDate);
+document.write("<br>");
+document.write("<br>");
+document.write("You are " + height + " your age is " + myAge + " weight: " + uWeight);
+document.write("<br>");
+document.write("<br>");
+document.write("Current location: " + locationCity + locationState + zip + " Housing type: " + housingType + " Occupation: " + occupation);
+document.write("<br>");
+document.write("<br>");
+document.write("Local time: " + localTime +  currentDate + " Previous Employer: " + prevEmployment + " Job Title: " + prevJobtitle + " Other experience: " + otherExperience);
+document.write("<br>");
+document.write("<br>");
+document.write("Thank you and " + goodbye);
+
+
